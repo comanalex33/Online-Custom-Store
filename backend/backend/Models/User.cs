f7a6488facf3b690868f5f3c15fa7c9a005987c9
Coman Alex <comanalex33@yahoo.com>
2021-10-16 02:18:43 +0300

    Backend added to project, database connected successfully with API

---
+++ backend/backend/Models/User.cs
@@ -0,0 +1,12 @@
+﻿
+namespace backend.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public string UserRole { get; set; }
+    }
+}
