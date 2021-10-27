6b813a00c4aa03b9e2dbc2d96567477509f332e8
Amalia Avramov <amalia.avramov@yahoo.com>
2021-10-27 17:57:12 +0300

    Backend for product in progress

--- backend/backend/Controllers/ProductController.cs
+++ backend/backend/Controllers/ProductController.cs
@@ -1,8 +1,18 @@
-﻿using backend.Models;
+﻿using backend.ModelRequest;
+using backend.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace backend.Controllers
 {
@@ -11,73 +21,90 @@ namespace backend.Controllers
         [ApiController]
         public class ProductController : ControllerBase
         {
-            private readonly IConfiguration _configuration;
+            private readonly AppDbContext _context;
+            private readonly IWebHostEnvironment _hostEnvironment;
 
-            public ProductController(IConfiguration configuration)
-            {
-                _configuration = configuration;
-            }
-
-            [HttpGet]
-            public JsonResult Get()
-            {
-                string query = @"
-                select ProductId as ""ProductId"",
-                       ProductName as ""ProductName"",
-                       ProductCategory as ""ProductCategory"",
-                       ProductDescription as ""ProductDescription"",
-                       ProductPrice as ""ProductPrice""
-                from Users
-            ";
-
-                DataTable table = new DataTable();
-                string sqlDataSource = _configuration.GetConnectionString("Database");
-                NpgsqlDataReader myReader;
-                using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
-                {
-                    myCon.Open();
-                    using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
-                    {
-                        myReader = myCommand.ExecuteReader();
-                        table.Load(myReader);
-
-                        myReader.Close();
-                        myCon.Close();
-                    }
-                }
-
-                return new JsonResult(table);
-            }
-
-            [HttpPost]
-            public JsonResult Post(Product product)
-            {
-                string query = @"
-                insert into products(ProductId,ProductName,ProductCategory,ProductDescription,ProductPrice)
-                values (@ProductId,@ProductName,@ProductCategory,@ProductDescription,@ProductPrice)
-            ";
-
-                DataTable table = new DataTable();
-                string sqlDataSource = _configuration.GetConnectionString("Database");
-                NpgsqlDataReader myReader;
-                using (NpgsqlConnection myCon = new NpgsqlConnection(sqlDataSource))
-                {
-                    myCon.Open();
-                    using (NpgsqlCommand myCommand = new NpgsqlCommand(query, myCon))
-                    {
-                        myCommand.Parameters.AddWithValue("@ProductName", product.ProductName);
-                        myCommand.Parameters.AddWithValue("@ProductCategory", product.ProductCategory);
-                        myCommand.Parameters.AddWithValue("@UserDescription", product.ProductDescription);
-                        myCommand.Parameters.AddWithValue("@UserPrice", product.ProductPrice);
-                        myReader = myCommand.ExecuteReader();
-                        table.Load(myReader);
-
-                        myReader.Close();
-                        myCon.Close();
-                    }
-                }
-
-                return new JsonResult("Added succesfully");
-            }
+        public ProductController(AppDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _hostEnvironment = hostEnvironment;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductModel>>> Get()
+        {
+            return await _context.Products
+                .Select(x => new ProductModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Category = x.Category,
+                    Description = x.Description,
+                    Price = x.Price,
+                    ImageName = x.ImageName,
+                    ImageSrc = (x.ImageName == null) ? null : String.Format("{0}://{1}{2}/Images/{3}", Request.Scheme, Request.Host, Request.PathBase, x.ImageName),
+                    UpdateImage = x.UpdateImage
+                })
+                .ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserModel>> PostTodoItem(UserRequestModel requestUser)
+        {
+            long Id = _context.Users.Count() + 1;
+            UserModel user = new UserModel(Id, requestUser);
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutTodoItem([FromForm] ProductModel user)
+        {
+            if (user.ImageFile != null)
+            {
+                user.ImageName = await SaveImage(user.ImageFile);
+            }
+            else if (user.UpdateImage == true)
+            {
+                user.ImageSrc = null;
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(user.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool UserExists(long id) => _context.Users.Any(e => e.Id == id);
+
+        [NonAction]
+        public async Task<string> SaveImage(IFormFile imageFile)
+        {
+            string imageName = new string(Path.GetFileNameWithoutExtension(imageFile.FileName).Take(10).ToArray()).Replace(' ', '-');
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(imageFile.FileName);
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, "Images", imageName);
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+            return imageName;
+        }
+    }
     }
